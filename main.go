056fd785ee969d013303e607c2f2574568a6b2e8
Daniel Esteban <conejo@theninjabunny.com>
2022-12-04 14:39:25 +0100

    code

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"image/color"
 	"machine"
+	"math/rand"
 	"time"
 
 	"tinygo.org/x/tinyfont/proggy"
@@ -15,9 +16,19 @@ import (
 
 var disp max8x8display.Device
 var white = color.RGBA{255, 255, 255, 255}
+var msgs = []string{
+	"HAPPY NEW YEAR",
+	"HAPPY HOLIDAYS",
+	"MERRY XMAS",
+}
+var w32 uint32
+var n int32
 
 func main() {
-	// Pins for Arduino Nano 33 IOT
+	// Let 1 second so all devices are powered correctly
+	time.Sleep(1 * time.Second)
+
+	// Initialize and configure the SPI connection
 	err := machine.SPI0.Configure(machine.SPIConfig{
 		SDO:       machine.D9, // default SDO pin
 		SCK:       machine.D8, // default sck pin
@@ -28,28 +39,37 @@ func main() {
 		println(err.Error())
 	}
 
+	// Create a new max7219 device, configure it as a 8x8 display
 	matrix := max72xx.NewDevice(*machine.SPI0, machine.D7)
-
 	disp = max8x8display.New(matrix)
 	disp.Configure(max8x8display.Config{})
 
+	// show nothing on the display, just in case
 	disp.ClearDisplay()
 	disp.Display()
 
-	dino()
-	heart()
-	snowing()
-	smile()
-	tree()
-
-	text := "TINYGO - MERRY XMAS"
-	w32, _ := tinyfont.LineWidth(&proggy.TinySZ8pt7b, text)
 	for {
-		for i := int16(8); i > int16(-w32); i-- {
-			disp.ClearDisplay()
-			tinyfont.WriteLine(&disp, &proggy.TinySZ8pt7b, i, 7, text, white)
-			disp.Display()
-			time.Sleep(250 * time.Millisecond)
+		n = rand.Int31n(7)
+		if n == 0 {
+			heart() // shows a heart icon
+		} else if n == 1 {
+			snowing() // shows a small snowing animation
+		} else if n == 2 {
+			smile() // shows a smiley icon
+		} else if n == 3 {
+			tree() // shows a tree icon
+		} else {
+			// display one of the text messages in a marquee mode
+			w32, _ = tinyfont.LineWidth(&proggy.TinySZ8pt7b, msgs[n-4])
+			for i := int16(8); i > int16(-w32); i-- {
+				disp.ClearDisplay()
+				tinyfont.WriteLine(&disp, &proggy.TinySZ8pt7b, i, 7, msgs[n-4], white)
+				disp.Display()
+				time.Sleep(125 * time.Millisecond)
+			}
 		}
+		disp.ClearDisplay()
+		disp.Display()
+		time.Sleep(5 * time.Second)
 	}
 }
